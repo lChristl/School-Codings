ca2e0ded723b299ccf75e2344c6b9cacafa3b88d
Christian De Leon <145353689+lChristl@users.noreply.github.com>
2024-03-05 12:00:24 +0800

    the 2 activity
    edit later

---
+++ Focalor.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.focalor;
+
+/**
+ *
+ * @author Christian F. De leon
+ */
+public class Focalor {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
